9b20be62b0b65d1a1e606c66b5ba2ec3b2e7b4e9
Kirill Tregubov <contact@kirilltregubov.com>
2023-09-26 21:45:02 -0400

    changes

--- src/pages/content/App.tsx
+++ src/pages/content/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="fixed bottom-0 left-0 z-50 bg-amber-400 text-lg text-black">
+    <div className="fixed bottom-0 left-0 z-[1000] bg-amber-400 text-lg text-black">
       content script loaded
     </div>
   )
--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,6 +1,6 @@
 export default function Options() {
   return (
-    <div className="flex h-[50vh] w-full items-center justify-center text-3xl">
+    <div className="flex w-full items-center justify-center py-6 text-3xl">
       Options
     </div>
   )
